c4c546de314f495afd35665a67b156f4ce4f1ad8
Peter <pvdochtend@gmail.com>
2019-07-03 11:28:32 +0200

    Add files via upload

--- webview.js
+++ webview.js
@@ -1,1 +1,21 @@
-'use strict';
+"use strict";
+
+module.exports = Franz => {
+  const getMessages = function getMessages() {
+    let count = 0;
+
+    if (document.getElementsByClassName('updates-widget')[0].querySelectorAll("button").length > 0) {
+      count = parseInt(document.getElementsByClassName('updates-widget')[0].querySelectorAll("button")[0].innerHTML.replace(/[^0-9.]/g, ''),10);
+    }
+
+    count = parseInt(count, 10);
+
+    if (isNaN(count)) {
+      count = 0;
+    }
+
+    Franz.setBadge(count);
+  };
+
+  Franz.loop(getMessages);
+};
